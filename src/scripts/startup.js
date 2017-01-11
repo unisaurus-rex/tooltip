bd9fa6774cc15849edba90f702f3971e8a0685cf
Asterisk Loftis <asterisk@asteriskloftis.com>
2017-01-11 16:01:37 -0700

    Dear Gawd.

--- src/scripts/startup.js
+++ src/scripts/startup.js
@@ -7,23 +7,19 @@ import * as d3 from "d3";
 import {toolTips} from 'tooltips';
 
 
-
 //The global variables
 var justTheTip = toolTips();
 
-
-
 //Fakey-fake data 
-
 var data = [
-    { x: 20, y: 40, name: "Booty"},
-    { x: 50, y: 70, name: "bootaay"},
-    { x: 100, y: 120, name: "butt-cheeks!"}
+    { x: 20, y: 40, name: "Here are my data values:"},
+    { x: 50, y: 70, name: "Oh snap"},
+    { x: 100, y: 120, name: "This is a very long tool tip to see what happens when its long AF"}
 ];
 
 //Let's make an quicky-quick svg example in this mofo
 //set the svg stage for utter magic
-var exsvg = d3.select("#example")
+var exsvg = d3.selectAll(".panel-body")
 .append('svg')
 .attr('width', "200px")
 .attr('height', "200px")
@@ -37,7 +33,7 @@ exsvg.selectAll("circle").data(data, function(d) { return d.name;})
 .attr("cx", function(d) { return d.x; })
 .attr("cy", function(d) { return d.y;})
 .attr("r", 10).attr("fill", "#fff")
-.attr("title",function(d) {return d.name;}) //this is important to use data (from name key) to assign the title for the circles
+.attr("title",function(d) {return d.name + " X val:" + d.x + " Y val:" +d.y;}) //this is important to use data (from name key) to assign the title for the circles, this is how tooltips will work with svgs
 .attr("class","my_circle");
 
 justTheTip;
--- src/scripts/utils/tooltips.js
+++ src/scripts/utils/tooltips.js
@@ -1,18 +1,28 @@
 import * as d3 from "d3";
 
 export function toolTips() {
- 
- //tooltips are opt-in so they need to be manually initialized, I suppose this could be in start up too
-$(document).ready(function () {
-    $('[data-toggle="tooltip"]').tooltip({
-        //'placement': 'right'
-    });
-    
-    $("svg, svg circle").tooltip({
-        'container': 'body',
-        'placement': 'right'
-    });
-});
 
-    console.log("Poop train");
+    //tooltips are opt-in so they need to be manually initialized, I suppose this could be in start up too
+
+    $(document).ready(function () {
+
+        $("body").tooltip({ selector: '[title]' });
+
+        $("[title]").tooltip({
+            container: 'body',
+            placement: 'top',
+            title: 'No Value',
+            trigger: 'click'
+        });
+        //alter the tooltip position when shown
+        $('[title]').click(function () {
+            //var offset = this.offset();
+            $('.tooltip').css('top', parseInt($('.tooltip').css('top')) + $('body').scrollTop() + 'px')
+        });
+
+        console.log("Poop train");
+        window.d3 = d3;
+    });
+
+
 }
