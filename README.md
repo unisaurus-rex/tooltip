b952c41030c5e58450299dfbdb3e1bca0675956f
Asterisk Loftis <asterisk@asteriskloftis.com>
2017-01-13 07:34:37 -0700

    Updating documentation

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Tooltip Module 
-This repo contains the  tool tip module. This repo has a Bootstrap 3 tooltip and a purely d3 v4 tooltip.
-
-## Botstrap Tooltip
+This repo contains the  tooltip module. This module uses Boostrap 3's tooltip component  as well as a custom tool tip that works to pull data from our custom SVG elements.
 
 
-
+### Using The Botstrap Tooltip
+The Bootstrap template is simple to use just add the data-toggle="tooltip" attribute to any element you'd like a tooltip to pop up. This will activate the tooltip and use the title attribute as the text. See below:
 ```javascript
 <!-- HTML to write -->
 <a href="#" data-toggle="tooltip" title="Some tooltip text!">Hover over me</a>
@@ -18,9 +17,20 @@ This repo contains the  tool tip module. This repo has a Bootstrap 3 tooltip and
 </div>
 ```
 
+### Using The Custom SVG Tooltip
+The custom SVG tooltip works in 2 parts and basically adds an event to the mouse pointer  that activates when hovering over a SVG element. See below:
 
 
+**Step 1:** Add the code below to the D3 svg element you are appending. Basically you just want to make sure the title attribute has a value pulled from the data use to draw the element. In this case we are using it's value:
 ```javascript
-<!-- HTML to write -->
  .attr("title",function(d) {return d.value}) 
+```
+**Step 2:** Add the code below to the the body of your HTML page. Make sure it isn't nested in anything, it's probably easiest to just throw it at the bottom:
+```javascript
+<!--This is the hidden mouse pointer tooltip-->
+<i id="svg-tooltip" data-toggle="tooltip" data-placement="auto" title="Tooltip for svg elements" data-animation="false" data-trigger="manual"/>
+```
+**Step 3:** Add the code to your main JS file to import the module ES6 style:
+```javascript
+import {toolTips} from 'tooltips';
 ```
--- src/scripts/utils/tooltips.js
+++ src/scripts/utils/tooltips.js
@@ -2,37 +2,26 @@ import * as d3 from "d3";
 
 export function toolTips() {
 
-    //tooltips are opt-in so they need to be manually initialized, I suppose this could be in start up too
-
     $(document).ready(function () {
 
 
-// Part #1 Uses a hidden tooltip with id svg-tooltip to place tooltip on mouse pointer on mouse move and leave events for selections
+        // Part #1 Opt-in for bootstrap tool tip with a few options, this is for any element that is NOT a SVG drawing. The element will need to have the [data-toggle="tooltip"] attribute to work
         $('[data-toggle="tooltip"]').tooltip({
             container: 'body',
             placement: 'auto',
             title: 'No Value',
         });
 
-
-
-
         // Part #2 Uses a hidden tooltip with id svg-tooltip to place tooltip on mouse pointer on mouse move and leave events for selections
-        $("circle, rect, path").on('mousemove', function (e) {
-            $("#svg-tooltip").css({ top: e.pageY + $('body').scrollTop(), left: e.pageX }); //top is event y position plus the scroll postion of body
-            $('#svg-tooltip').prop('title', $(this).attr('title')); //Sets the tilte attribute of the 
-            $('#svg-tooltip').tooltip('show')
+        $("circle, rect, path").on('mousemove', function (e) { //Elements to select for the tooltip function,as we add new ones we need to update
+            $("#svg-tooltip").css({ top: e.pageY + $('body').scrollTop(), left: e.pageX });     //top is event y position plus the scroll postion of body
+            $('#svg-tooltip').prop('title', $(this).attr('title'));     //Sets the tilte attribute of the element the pointer is on 
+            $('#svg-tooltip').tooltip('show')   //Shows the tooltip LAST to reduce flicker and jumping
         })
 
         $("circle, rect, path").on('mouseleave', function (e) {
             $('#svg-tooltip').tooltip('destroy')
-            $('#svg-tooltip').prop('title', 'Test');
-
         })
-
-        console.log("Poop train");
-        window.d3 = d3;
-
 
     });
 
