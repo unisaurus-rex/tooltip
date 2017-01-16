30948b975c8da98af7770d3b538d5b634293e8bb
Asterisk Loftis <asterisk@asteriskloftis.com>
2017-01-15 20:34:21 -0700

    Added cross browser compatible functions

--- src/scripts/utils/tooltips.js
+++ src/scripts/utils/tooltips.js
@@ -4,6 +4,21 @@ export function toolTips() {
 
     $(document).ready(function () {
 
+        //Part 0  Due to cross browser inconsistency you need to normalize the scrollTop/pageYOffset pixels
+        function getScrollTop() {
+            if (typeof pageYOffset != 'undefined') {
+                //most browsers except IE before #9
+                return pageYOffset;
+            }
+            else {
+                var B = document.body; //Chrome and IE 'quirks' mode use the body as the first measured element
+                var D = document.documentElement; //IE with doctype, because the documentElement is the first scroll measured element THANKS IE
+                D = (D.clientHeight) ? D : B; //If D has a clientHieght (true) then use D as defined above, else D will equal B as described above... just a fancy inline if else
+                return D.scrollTop;
+            }
+        }
+
+        console.log(getScrollTop());
 
         // Part #1 Opt-in for bootstrap tool tip with a few options, this is for any element that is NOT a SVG drawing. The element will need to have the [data-toggle="tooltip"] attribute to work
         $('[data-toggle="tooltip"]').tooltip({
@@ -14,13 +29,13 @@ export function toolTips() {
 
         // Part #2 Uses a hidden tooltip with id svg-tooltip to place tooltip on mouse pointer on mouse move and leave events for selections
         $("circle, rect, path").on('mousemove', function (e) { //Elements to select for the tooltip function,as we add new ones we need to update
-            $("#svg-tooltip").css({ top: e.pageY + $('body').scrollTop(), left: e.pageX });     //top is event y position plus the scroll postion of body
+            $("#svg-tooltip").css({ top: e.pageY + getScrollTop(), left: e.pageX });     //css absolute top is event y position plus the scroll postion of bodyn x is event x position
             $('#svg-tooltip').prop('title', $(this).attr('title'));     //Sets the tilte attribute of the element the pointer is on 
             $('#svg-tooltip').tooltip('show')   //Shows the tooltip LAST to reduce flicker and jumping
         })
 
         $("circle, rect, path").on('mouseleave', function (e) {
-            $('#svg-tooltip').tooltip('destroy')
+            $('#svg-tooltip').tooltip('destroy') //cannot simply 'hide' this because it will take a hit to performance as well as confuse the tooltip
         })
 
     });
